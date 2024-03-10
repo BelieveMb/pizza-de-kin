147c50ccad443ae94e2537293a41b329254af532
Dev Unbelievable <mbolokobelieve@gmail.com>
2024-03-10 22:13:37 +0100

    restructuration of folder

---
+++ src/pages/Home/Page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Page() {
+  return (
+    <>
+        <Header/>
+    
+    </>
+    
+
+  )
+}
+
+export default Page
